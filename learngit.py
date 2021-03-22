954f3264e43e0591082815ba403fd29ca682ba81
Maros Tomasov <maros3845@gmail.com>
2021-03-22 20:37:08 +0100

    Update learngit.py

--- learngit.py
+++ learngit.py
@@ -10,10 +10,10 @@ browser = webdriver.Chrome(chromedriver)
 browser.get("https://github.com/new")
 login = browser.find_element_by_xpath("//*[@id='login_field']")
 login.click()
-login.send_keys('zekosnb')
+login.send_keys('') # enter your username
 password = browser.find_element_by_xpath("//*[@id='password']")
 password.click()
-password.send_keys('MakoMako132')
+password.send_keys('')# enter your password
 kk = browser.find_elements_by_name('commit')
 kk[0].click()
 repo = browser.find_elements_by_xpath("//*[@id='repository_name']")
@@ -27,10 +27,10 @@ windows = browser.window_handles
 browser.switch_to.window(windows[1])
 loginstack = browser.find_element_by_name("email")
 loginstack.click()
-loginstack.send_keys('maros3845@gmail.com')
+loginstack.send_keys('')# enter your login
 passwordstack = browser.find_element_by_xpath("//*[@id='password']")
 passwordstack.click()
-passwordstack.send_keys('MakoMako132') 
+passwordstack.send_keys('')# enter your password 
 python_button = browser.find_element_by_xpath("//*[@id='submit-button']")
 python_button.click()
 
